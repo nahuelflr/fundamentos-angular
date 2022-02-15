519854e894dbd969747857549b9901872ae94a95
Nahuel Flores <nahuelflr@gmail.com>
2022-02-14 23:45:43 -0300

    agregado de ngFor con objetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,6 +20,41 @@ export class AppComponent {
 
   names: string[] = ['Nahuel', 'Julian', 'Santi'];
   newName = '';
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 3434,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 343,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+
+
 
   toggleButton() {
     this.btnDisable = !this.btnDisable;
